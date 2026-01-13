5f43b76b4956567e7ba4e42e33984eae5dd7725d
Andrew Dainty <genmailing@gmail.com>
2026-01-14 08:51:54 +1100

    chore: exclude worktrees from Jest and add anthropic sdk

--- jest.config.js
+++ jest.config.js
@@ -16,6 +16,10 @@ const customJestConfig = {
     '**/__tests__/**/*.test.ts',
     '**/__tests__/**/*.test.tsx',
   ],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/.worktrees/',
+  ],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
